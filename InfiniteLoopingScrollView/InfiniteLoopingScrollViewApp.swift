25ac0ffb1c30dea4f632f25bbe63c0118f0a6571
IvanL <antila3567@gmail.com>
2023-12-27 13:21:23 +0200

    Initial Commit

---
+++ InfiniteLoopingScrollView/InfiniteLoopingScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteLoopingScrollViewApp.swift
+//  InfiniteLoopingScrollView
+//
+//  Created by Иван Легенький on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteLoopingScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
